fede9162fecfc2b9f557455e3300f656c57a8e6e
RalphLAtGitHub <mail@ralphlechterbeck.de>
2021-03-26 14:04:39 +0100

    Update test.js

--- WebView2/WebResources/test.js
+++ WebView2/WebResources/test.js
@@ -18,6 +18,6 @@ var view = new ol.View({
 map.setView(view);
 
 function setCenterAt(lat, lon) {
-    console.log (">>> Message received from NET: Setting center of map.")
+    console.log (">>> Message received from .NET: Setting center of map.")
     view.setCenter(ol.proj.transform([lon, lat], "EPSG:4326", "EPSG:3857"));
 }
