2cac1279908ff40890620fe74d94d7fe9a0f6e8d
RalphLAtGitHub <mail@ralphlechterbeck.de>
2021-03-29 09:26:28 +0200

    Update ApplicationEvents.vb

--- WebView2/ApplicationEvents.vb
+++ WebView2/ApplicationEvents.vb
@@ -4,7 +4,12 @@
 
         Private Sub MyApplication_UnhandledException(ByVal sender As Object, ByVal e As ApplicationServices.UnhandledExceptionEventArgs) Handles Me.UnhandledException
 
-            Debug.WriteLine(vbCrLf + e.Exception.Message + vbCrLf + e.Exception.Source + vbCrLf + e.Exception.StackTrace)
+           If e.Exception.Source = "Microsoft.Web.WebView2.WinForms" Then
+
+                Debug.WriteLine(vbCrLf + e.Exception.Message + vbCrLf + e.Exception.Source + vbCrLf + e.Exception.StackTrace)
+                e.ExitApplication = False
+
+            End If
 
         End Sub
 
